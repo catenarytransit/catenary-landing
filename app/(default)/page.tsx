4f71a807a0133209ce68f4a34e93a682c56d55b6
Sam Jeffs <sam@quacksire.dev>
2023-10-10 00:37:45 -0700

    open graph

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -2,11 +2,6 @@ import Hero from '@/components/hero'
 import OpenSourceList from '@/components/open-source-list'
 import Tabs from '@/components/tabs'
 
-export const metadata = {
-    title: 'Home - Open PRO',
-    description: 'Page description',
-}
-
 export default function Home() {
     return (
         <div className="flex flex-col divide-y divide-solid">
