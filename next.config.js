b2d3ab6493be21cd82e9d479c6cde31b615b9f6b
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-10-10 10:30:48 -0700

    redirect paths

--- next.config.js
+++ next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    async redirects() {
+        return [
+          {
+            source: '/maps',
+            destination: 'https://maps.catenarymaps.org',
+            permanent: true,
+          },
+          {
+            source: '/map',
+            destination: 'https://maps.catenarymaps.org',
+            permanent: true,
+          },
+        ]
+      }
+}
 
 module.exports = nextConfig
