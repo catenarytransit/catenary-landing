85ba6653f721ee2ee678897c40c60fea91c9a869
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2023-10-14 00:42:37 -0700

    add gtm

--- app/(default)/layout.tsx
+++ app/(default)/layout.tsx
@@ -6,6 +6,14 @@ import AOS from 'aos'
 import 'aos/dist/aos.css'
 import { useEffect } from 'react'
 
+import TagManager from 'react-gtm-module'
+ 
+const tagManagerArgs = {
+    gtmId: 'GTM-WD62NKLX'
+}
+ 
+TagManager.initialize(tagManagerArgs)
+
 export default function DefaultLayout({ children }: { children: React.ReactNode }) {
     useEffect(() => {
         AOS.init({
