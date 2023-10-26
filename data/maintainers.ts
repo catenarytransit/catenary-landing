7eed5ec31f86efde05d964bc959823e81cddfb6a
lolpro11 <89674666+lolpro11@users.noreply.github.com>
2023-10-26 13:23:40 -0700

    Update maintainers.ts

--- data/maintainers.ts
+++ data/maintainers.ts
@@ -13,7 +13,7 @@ export const maintainers: Person[] = [
         pronouns: 'he/him',
         school: 'University of California, Irvine',
         src: 'https://github.com/kylerchin.png',
-        link: 'https://kylerchin.comhttps://kylerchin.com/',
+        link: 'https://kylerchin.com',
         title: 'Boardmember, Founder, Director, Systems Programmer, Algorithms, Frontend, Design',
     },
     {
@@ -22,7 +22,7 @@ export const maintainers: Person[] = [
         school: 'California State University, Los Angeles',
         src: 'https://github.com/CatenaryMaps.png',
         link: 'https://github.com/CatenaryMaps',
-        title: 'Boardmember, Systems Architecture, Algorithms',
+        title: 'Boardmember, Sysadmin, Systems Architecture, Algorithms',
     },
     {
         name: 'Andrew Shen',
@@ -30,7 +30,15 @@ export const maintainers: Person[] = [
         school: 'University of California, Irvine',
         src: 'https://github.com/AndrewHC36.png',
         link: 'https://github.com/AndrewHC36',
-        title: 'Boardmember, Frontend/Flutter',
+        title: 'Boardmember, Frontend/Flutter, Algorithms, Machine Learning',
+    },
+    {
+        name: 'Josh Wong',
+        pronouns: 'he/him',
+        school: 'Santa Monica College & Don Bosco Technical Institute, Rosemead',
+        src: 'https://github.com/lolpro11.png',
+        link: 'https://github.com/lolpro11',
+        title: 'Director, Sysadmin, Systems Programmer, Algorithms',
     },
     {
         name: 'Samuel Sharp',
@@ -47,14 +55,6 @@ export const maintainers: Person[] = [
         src: 'https://github.com/BustosAndrew.png',
         link: 'https://github.com/BustosAndrew',
         title: 'Frontend/Flutter, User Design',
-    },
-    {
-        name: 'Josh Wong',
-        pronouns: 'he/him',
-        school: 'Santa Monica College & Don Bosco Technical Institute, Rosemead',
-        src: 'https://github.com/lolpro11.png',
-        link: 'https://github.com/lolpro11',
-        title: 'Director, Sysadmin, Systems Programmer, Algorithms',
     },
     {
         name: 'Jason T.',
