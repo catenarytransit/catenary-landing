eef0f87073bf1a8dcb1de26ae78fa59150eca78e
Kyler Chin <7539174+kylerchin@users.noreply.github.com>
2024-04-09 23:20:52 -0700

    V1 of TOS

---
+++ app/tos/page.tsx
@@ -0,0 +1,53 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function Page() {
+    return (
+        <div className=" divide-y divide-solid">
+            <section>
+                <div className="flex items-left relative pt-32 pb-12 md:pt-40 md:pb-16">
+                    <div className="container  px-4">
+                        <div className="">
+                            <span className="h4 mb-20">Catenary Maps Terms of Service</span>
+
+                                <p>
+                                ﻿
+
+TERMS OF SERVICE</p><p>
+Last updated April 10, 2024</p><p>
+AGREEMENT TO OUR LEGAL TERMS</p><p>
+We are Catenary Transit Initiatives, doing business as Catenary Maps and Catenary ('Company', 'we', 'us', or 'our').</p><p>
+We operate the website https://catenarymaps.org (the 'Site'), the mobile application Catenary Maps (the 'App'), as well as any other related products and services that refer or link to these legal terms (the 'Legal Terms') (collectively, the 'Services').</p><p>
+You can contact us by email at kyler@catenarymaps.org </p><p>
+These Legal Terms constitute a legally binding agreement made between you, whether personally or on behalf of an entity ('you'), and Catenary Transit Initiatives, concerning your access to and use of the Services. You agree that by accessing the Services, you have read, understood, and agreed to be bound by all of these Legal Terms. IF YOU DO NOT AGREE WITH ALL OF THESE LEGAL TERMS, THEN YOU ARE EXPRESSLY PROHIBITED FROM USING THE SERVICES AND YOU MUST DISCONTINUE USE IMMEDIATELY.
+Supplemental terms and conditions or documents that may be posted on the Services from time to time are hereby expressly incorporated herein by reference. We reserve the right, in our sole discretion, to make changes or modifications to these Legal Terms from time to time. We will alert you about any changes by updating the 'Last updated' date of these Legal Terms, and you waive any right to receive specific notice of each such change. It is your responsibility to periodically review these Legal Terms to stay informed of updates. You will be subject to, and will be deemed to have been made aware of and to have accepted, the changes in any revised Legal Terms by your continued use of the Services after the date such revised Legal Terms are posted.
+All users who are minors in the jurisdiction in which they reside (generally under the age of 18) must have the permission of, and be directly supervised by, their parent or guardian to use the Services. If you are a minor, you must have your parent or guardian read and agree to these Legal Terms prior to you using the Services.
+We recommend that you print a copy of these Legal Terms for your records.
+                               </p>
+
+                               <br/>
+
+        <span className="h3 mb-20">Our Services</span>
+            <p>
+            The information provided when using the Services is not intended for distribution to or use by any person or entity in any jurisdiction or country where such distribution or use would be contrary to law or regulation or which would subject us to any registration requirement within such jurisdiction or country. Accordingly, those persons who choose to access the Services from other locations do so on their own initiative and are solely responsible for compliance with local laws, if and to the extent local laws are applicable.
+<br/>The Services are not tailored to comply with industry-specific regulations (Health Insurance Portability and Accountability Act (HIPAA), Federal Information Security Management Act (FISMA), etc.), so if your interactions would be subjected to such laws, you may not use the Services. You may not use the Services in a way that would violate the Gramm-Leach-Bliley Act (GLBA).
+            </p>
+            <span className="h3 mb-20">Intellectual Property</span>
+            <p>
+            We are the owner or the licensee of all intellectual property rights in our Services, including all source code, databases, functionality, software, website designs, audio, video, text, photographs, and graphics in the Services (collectively, the "Content"), as well as the trademarks, service marks, and logos contained therein (the marks).
+
+            <br/>
+            Our Content and Marks are protected by copyright and trademark laws (and various
+other intellectual property rights and unfair competition laws) and treaties in the United
+States and around the world.
+            </p>
+                <p>The contents and marks are provided in or through the Services "AS IS" for your only.</p>
+                              </div>  
+                            </div>
+                        </div>
+                    
+                
+            </section>
+        </div>
+    )
+}
